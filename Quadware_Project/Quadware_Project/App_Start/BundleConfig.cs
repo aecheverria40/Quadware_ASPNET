7344401a0b449c260a7a1645de975ea0560fb6fe
Abiel Villegas <villegaspro64@gmail.com>
2019-07-26 18:12:09 -0700

    Initial commit

---
+++ Quadware_Project/Quadware_Project/App_Start/BundleConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Quadware_Project
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css"));
+
+            //Bootstrap Material Design
+            bundles.Add(new StyleBundle("~/Content/materialize").Include(
+                "~/Content/bootstrap-material-design/css/bootstrap-material-design.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundle/materializejs").Include(
+                "~/Content/bootstrap-material-design/js/bootstrap-material-design.js"));
+
+            bundles.Add(new ScriptBundle("~/bundle/popperjs").Include(
+                "~/Scripts/popper.js"));
+
+            //Material Desing Lite
+            bundles.Add(new StyleBundle("~/Content/mdl").Include(
+                "~/Content/mdl/material.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/mdljs").Include(
+                "~/Content/mdl/material.js"
+                ));
+        }
+    }
+}
---
+++ Quadware_Project/Quadware_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quadware_Project.MvcApplication" Language="C#" %>
